c7b47b47479221d100eef62fd133a22d3b57cf50
Samuel <samuel1skultety@gmail.com>
2023-01-07 12:38:45 +0100

    solved exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = 0;
+    let oldestPerson = {};
+    people.forEach(person => {
+        let age = getAge(person);
+        if (age > oldest) {
+        oldest = age;
+        oldestPerson = person;
+        }
+    });
+    return oldestPerson;
+};
 
+const getAge = function(person) {
+    let age = 0;
+    if (person.yearOfDeath) {
+        age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+        age = new Date().getFullYear() - person.yearOfBirth;
+    }
+    return age;
 };
 
 // Do not edit below this line
