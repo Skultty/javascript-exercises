a11ab7ea682e789077dd376bd57f6b97357a25c8
Samuel <samuel1skultety@gmail.com>
2023-01-07 11:00:27 +0100

    solved exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let str1 = str.toLowerCase().replace(/[^a-z]/g, "");
+    let str2 = str1.split("").reverse().join("");
+    return str1 === str2;
 };
 
 // Do not edit below this line
